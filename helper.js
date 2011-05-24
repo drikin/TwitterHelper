934a1264d48dee66d17fd13d56e386845a4c1206
Kohichi Aoki <drikin@gmail.com>
2011-05-24 08:25:21 -0700

    show unread number on action button

--- helper.js
+++ helper.js
@@ -1,8 +1,24 @@
 (function() {
+    function DOMNodeInserted() {
+        installEvent();
+        updateBadge();
+    }
+
     function installEvent() {
         var target = document.getElementById('new-tweets-bar');
         if (target) {
             target.addEventListener('mousedown', markAsRead, false);
+        }
+    }
+
+    function updateBadge() {
+        var target = document.getElementById('new-tweets-bar');
+        if (target) {
+            var tc = target.textContent;
+            var count = tc.match(/\d+/);
+            chrome.extension.sendRequest({type:"updateBadge", text:count[0]});
+        } else {
+            chrome.extension.sendRequest({type:"updateBadge", text:''});
         }
     }
 
@@ -13,6 +29,6 @@
         }
     }
 
-    document.addEventListener("DOMNodeInserted", installEvent, false);
+    document.addEventListener("DOMNodeInserted", DOMNodeInserted, false);
 })();
 // vim:set ts=4 sw=4 expandtab:
