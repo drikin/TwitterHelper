91e90083f1570070b7ba9c4a98125f2dd5dc05cb
drikin <drikin@gmail.com>
2011-05-26 21:26:14 -0700

    implement speech input

--- helper.js
+++ helper.js
@@ -1,4 +1,40 @@
 (function() {
+
+    function setLocale(lo) {
+        var h = document.getElementsByTagName('html')[0];
+        h.lang = lo;
+    }
+
+    function installSpeechInput() {
+        var t = document.getElementsByClassName('text-area-editor twttr-editor')[0];
+        if (typeof t !== 'undefined') {
+            document.removeEventListener('DOMNodeInserted', installSpeechInput);
+
+            var i = document.createElement('input');
+            t.appendChild(i);
+
+            i.id = 'twp-speech-input';
+            i.setAttribute('x-Webkit-Speech');
+            i.setAttribute('autocomplete', 'off');
+            i.setAttribute('autocorrect', 'off');
+            i.onwebkitspeechchange = speechChange;
+            i.style.border = '0px';
+            i.style.top = '2px';
+            i.style.right = '-25px';
+            i.style.position = 'absolute';
+            i.style.color = 'rgba(0, 0, 255, 0)';
+            i.style.background = 'rgba(0, 0, 255, 0)';
+            i.style.outline = 'none';
+            i.style.fontSize = '20px';
+            i.style.width = '20px';
+        }
+    }
+
+    function speechChange() {
+        var t = document.getElementsByClassName('twitter-anywhere-tweet-box-editor')[0];
+        t.value += document.getElementById("twp-speech-input").value;
+    }
+
     function DOMNodeInserted() {
         installEvent();
         updateBadge();
@@ -37,5 +73,8 @@
     });
 
     document.addEventListener("DOMNodeInserted", DOMNodeInserted, false);
+    document.addEventListener("DOMNodeInserted", installSpeechInput, false);
+
+    setLocale('ja');
 })();
 // vim:set ts=4 sw=4 expandtab:
